e5072fb0e00630394ee28a607dfd3fa49601b0db
Balaraju Kadukuntla <balaraju004@gmail.com>
2025-06-02 08:17:58 -0400

    Longest common prefix

---
+++ src/main/java/practice/LongestCommonPrefix.java
@@ -0,0 +1,36 @@
+package practice;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        String[] strs = new String[]{"flower","flow","flight"};
+        System.out.println(new LongestCommonPrefix().longestCommonPrefix(strs));
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+
+        String first = strs[0];
+        StringBuilder res = new StringBuilder();
+        for(int i=0;i<first.length();i++){
+            char ch = first.charAt(i);
+            if(!hasMatch(strs, i, ch)){
+                break;
+            }
+            res.append(ch);
+        }
+
+        return res.toString();
+
+    }
+
+    private boolean hasMatch(String[] strs, int i, char ch){
+
+
+        for(int j=0;j<strs.length;j++){
+            if(i >= strs[j].length() || strs[j].charAt(i)!=ch){
+                return false;
+            }
+        }
+        return true;
+    }
+}
